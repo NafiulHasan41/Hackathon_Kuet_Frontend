f6f36ecc8c7ac11c4558d8da273a9934101394cd
Asif Mahmud <as.if.98914@gmail.com>
2025-01-03 23:14:00 +0600

    user profile added

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,11 +1,17 @@
 "use client"
 
 import PdfConverter from "@/components/pdfconverter/pdfConverter";
+import Profile from "@/components/profile/profile";
 
 export default function page(){
+
     return(
-        <div>
+        <>
+        <div >
             <PdfConverter/>
         </div>
+        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row gap-4">
+        </div>
+        </>
     )
 }
