bdf39f82581fea5f8dac17c0cef74e4ac302c301
NafiulHasan41 <nhashik40@gmail.com>
2025-01-03 19:16:36 +0600

    Created user dashboard

---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import PdfConverter from "@/components/pdfconverter/pdfConverter";
+
+export default function page(){
+    return(
+        <div>
+            <PdfConverter/>
+        </div>
+    )
+}
