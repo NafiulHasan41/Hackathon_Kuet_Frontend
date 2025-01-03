de15e940e5b97502ffad298ed164a91998086c43
Asif Mahmud <as.if.98914@gmail.com>
2025-01-03 23:32:44 +0600

    profileCard initialized

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -4,6 +4,13 @@ import PdfConverter from "@/components/pdfconverter/pdfConverter";
 import Profile from "@/components/profile/profile";
 
 export default function page(){
+    const profile={
+        userName: "Swagoto",
+        userEmail: "swagoto@gmail.com",
+        wordsTranslated: 560,
+        storiesWritten: 3,
+        chatbotInteractions: 12,
+    }
 
     return(
         <>
@@ -11,6 +18,7 @@ export default function page(){
             <PdfConverter/>
         </div>
         <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row gap-4">
+            <Profile key={profile.userEmail} {...profile}></Profile>
         </div>
         </>
     )
---
+++ src/components/profilecard/profilecard.tsx
@@ -0,0 +1,18 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Car } from "lucide-react"
+
+  
+  export default function ProfileCard(){
+    return(
+        <Card>
+            
+        </Card>
+    )
+  }
